1ac8f3c4c689cf858bf3ec0f347917d8946527be
napalm1970 <nap1970@gmail.com>
2015-12-24 11:28:53 +0200

    .emacs.d added.

---
+++ .emacs.d/elpa/dracula-theme-1.2.0/dracula-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "dracula-theme" "1.2.0" "Dracula Theme" '((emacs "24")) :url "https://github.com/zenorocha/dracula-theme")
---
+++ .emacs.d/elpa/ido-yes-or-no-1.2/ido-yes-or-no-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ido-yes-or-no" "1.2" "Use Ido to answer yes-or-no questions" 'nil :url "https://github.com/DarwinAwardWinner/ido-yes-or-no")
