d019799a50aa00a3f29f9b4b0d8bdfdc80cf04d9
napalm1970 <napalm1970@gmail.com>
2021-01-05 20:13:59 +0200

    Add settings.

--- init.el
+++ init.el
@@ -12,10 +12,11 @@
 (unless (package-installed-p 'use-package)
 	(package-refresh-contents)
 	(package-install 'use-package))
-
+(setq use-package-always-ensure 1)
 (setq byte-compile-warnings '(cl-functions))
 ;; (setq inferior-lisp-program (executable-find "sbcl"))
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(setq message-log-max 10000)
 
 ;; (custom-set-variables
 ;;  ;; custom-set-variables was added by Custom.
@@ -42,10 +43,14 @@
    '("08ef1356470a9d3bf363ffab0705d90f8a492796e9db489936de4bde6a4fdb19" "bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" default))
  '(helm-minibuffer-history-key "M-p")
  '(inferior-lisp-program "/usr/bin/sbcl")
+ '(java-mode-hook
+   '(meghanada-mode flycheck-mode
+		    (lambda nil
+		      (setq c-basic-offset 2))))
  '(lisp-source-modes '(lisp-mode common-lisp-mode\
 ))
  '(package-selected-packages
-   '(meghanada sly-repl-ansi-color sly-macrostep sly company-lsp lsp-java lsp-ui lsp-mode platformio-mode jedi evil-collection company-emacs-eclim eclim evil-leader evil qt-pro-mode projectile magit expand-region auto-yasnippet auto-complete company-c-headers compamy-c-headers spaceline zerodark-theme sdcv dashboard monokai-theme color-themes smartparens yasnippet-snippets window-numbering which-key use-package try tabbar stickyfunc-enhance rainbow-delimiters paredit org-bullets neotree hungry-delete helm-themes helm-swoop helm-gtags helm-cscope gruvbox-theme ggtags flycheck dracula-theme disaster dired-quick-sort company-quickhelp cmake-mode clang-format cider beacon ace-window))
+   '(gitconfig-mode helm-flycheck undo-tree ws-butler evil-nerd-commenter super-save parinfer spacegray-theme eshell-syntax-highlighting fish-completion esh-autosuggest eshell-toggle meghanada sly-repl-ansi-color sly-macrostep sly company-lsp lsp-java lsp-ui lsp-mode platformio-mode jedi evil-collection company-emacs-eclim eclim evil-leader evil qt-pro-mode projectile magit expand-region auto-yasnippet auto-complete company-c-headers compamy-c-headers spaceline zerodark-theme sdcv dashboard monokai-theme color-themes smartparens yasnippet-snippets window-numbering which-key use-package try tabbar stickyfunc-enhance rainbow-delimiters paredit org-bullets neotree hungry-delete helm-themes helm-swoop helm-gtags helm-cscope gruvbox-theme ggtags flycheck dracula-theme disaster dired-quick-sort company-quickhelp cmake-mode clang-format cider beacon ace-window))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -53,4 +58,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Droid Sans Mono Dotted for Powerline" :foundry "1ASC" :slant normal :weight normal :height 128 :width normal)))))
+ '(default ((t (:family "Fira Code" :foundry "CTDB" :slant normal :weight normal :height 113 :width normal)))))
